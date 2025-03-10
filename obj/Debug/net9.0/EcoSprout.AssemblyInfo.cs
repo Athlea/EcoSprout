84136b7928b273efb61308f516322b9b2cf179f1
Athlea E. Baril <athlea.baril@cit.edu>
2025-03-11 00:18:46 +0800

    first-version merge

--- obj/Debug/net9.0/EcoSprout.AssemblyInfo.cs
+++ obj/Debug/net9.0/EcoSprout.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcoSprout")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b71bb29eca9f58bde54351cb880756968577651e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+653edc9b771169f651dce014b8b78b113b010b08")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcoSprout")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcoSprout")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
